16de7526542ce5b17e96ae02993accc697a6f825
Catiflam <gilfrid1@gmail.com>
2023-07-21 17:28:36 +0200

    edit for

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-for (let i = 1; i <= 10; i++) {
-	console.log("giro " + i);
+const myList = document.getElementById("my-list");
+
+for (let i = 1; i <= 100; i++) {
+	myList.innerHTML += `<li>Item ${i}</li>`;
+
+	if (i % 3 == 0) {
+		myList.innerHTML += `<li>Item ${i}</li>`;
+	}
 }
