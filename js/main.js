762e8c99807b99fb1534b95fe63178d7d096b588
Catiflam <gilfrid1@gmail.com>
2023-07-24 10:14:31 +0200

    edit fix

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const myList = document.getElementById("my-list");
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
 	// `
 	const listItem = document.createElement("li");
 	listItem.innerHTML = i;
 	if (i % 15 == 0) {
-		listItem.innerHTML = "fizz";
-	} else if (i % 3 == 0) {
-		listItem.innerHTML = "buzz";
-	} else if (i % 5 == 0) {
 		listItem.innerHTML = "fizzbuzz";
+	} else if (i % 3 == 0) {
+		listItem.innerHTML = "fizz";
+	} else if (i % 5 == 0) {
+		listItem.innerHTML = "buzz";
 	}
 
 	myList.appendChild(listItem);
